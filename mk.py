1ceaa42db59a80b1644724b55371b0c9fca1e814
TuanHoang1229 <122627428+TuanHoang1229@users.noreply.github.com>
2025-05-11 14:50:19 +0700

    Update mk.py

--- mk.py
+++ mk.py
@@ -107,6 +107,7 @@ Trang web này được xây dựng nhằm hỗ trợ học sinh THPT học tậ
 > – **Ray Clifford**
 """)
 
+    # CSS tùy chỉnh
     st.markdown("""
         <style>
         .box {
@@ -133,26 +134,26 @@ Trang web này được xây dựng nhằm hỗ trợ học sinh THPT học tậ
 
     # Nội dung "Liên hệ + Góp ý"
     st.markdown('<div class="box">', unsafe_allow_html=True)
-    
+
     # Thông tin liên hệ
     st.markdown("<h3>Liên hệ</h3>", unsafe_allow_html=True)
     st.markdown('<div class="contact-item">📍 <strong>Địa chỉ:</strong> Trường THPT Lương Văn Tri</div>', unsafe_allow_html=True)
     st.markdown('<div class="contact-item">📧 <strong>Email:</strong> ContactLVT@edu.vn</div>', unsafe_allow_html=True)
     st.markdown('<div class="contact-item">📞 <strong>Số điện thoại:</strong> 0966 813 528</div>', unsafe_allow_html=True)
     st.markdown('<div class="contact-item">👤 <strong>Người thực hiện:</strong> Hoàng Minh Tuấn</div>', unsafe_allow_html=True)
-    
-    
+
     # Góp ý kiến
     st.markdown("<h3 style='margin-top:40px;'>Góp ý kiến</h3>", unsafe_allow_html=True)
     feedback = st.text_area("Nhập ý kiến của bạn", height=120, label_visibility="collapsed")
-    
-    elif st.button("Gửi"):
+
+    if st.button("Gửi"):
         if feedback.strip():
             st.success("✅ Cảm ơn bạn đã góp ý!")
         else:
             st.warning("⚠️ Vui lòng nhập nội dung góp ý.")
-    
+
     st.markdown('</div>', unsafe_allow_html=True)
+
 
 # --- Kiểm tra mật khẩu ---
 elif selected_topic == "🔑 Kiểm tra mật khẩu":
